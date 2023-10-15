ceda4c0fb961db91f864496b6535ff4309c79849
Sarvesh Mehta <sarveshmehta1504@gmail.com>
2023-10-15 16:56:00 +0530

    experimental commit

--- app/src/main/java/com/example/git/MainActivity.kt
+++ app/src/main/java/com/example/git/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("This is in my second commit")
+        println("LOCAL CHANGES")
+        println("Experimental")
     }
 }
